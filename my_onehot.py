ac5fe2b09af40996c5dfabe61d9bfdd91afb984e
Mingyou Sung <mingyousung@gmail.com>
2022-05-02 13:34:52 -0400

    Update my_onehot.py

--- my_onehot.py
+++ my_onehot.py
@@ -1,16 +1,17 @@
 import torch
+from sklearn import preprocessing
+from text_processing import get_nlp_pipeline, word_tokenization
 
 def get_tokens(text, _pipeline):
-    nlp_pipeline = "spacy"
     selected_nlp_pipeline = get_nlp_pipeline(_pipeline)
 
-    return text_preprocessing(text, selected_nlp_pipeline, _pipeline)
+    return word_tokenization(text, selected_nlp_pipeline, _pipeline)
 
 def init_token2idx(text_list, _pipeline):
     selected_nlp_pipeline = get_nlp_pipeline(_pipeline)
     tk_lists = []
     for text in text_list:
-        tk_lists.append(text_preprocessing(text, selected_nlp_pipeline, nlp_pipeline))
+        tk_lists.append(word_tokenization(text, selected_nlp_pipeline, _pipeline))
         
     whole_tokens = [x for tk_list in tk_lists for x in tk_list]
     set_of_tokens = list(set(whole_tokens)) + ["UNK"]
@@ -27,7 +28,7 @@ def init_token2idx(text_list, _pipeline):
 
 def tk2idx(_text, _pipeline, token2idx_dict, unk_ignore):
     selected_nlp_pipeline = get_nlp_pipeline(_pipeline)
-    tk_lists = text_preprocessing(_text, selected_nlp_pipeline, _pipeline)
+    tk_lists = word_tokenization(_text, selected_nlp_pipeline, _pipeline)
         
     
     idx_list = []
@@ -52,11 +53,18 @@ def custom_one_hot_encoding(_idx_list, dim):
         
     return tensor_list
 
-def tensor2token(_tensor):
+def tensor2token(_tensor, idx2token_dict):
     idx = (_tensor == 1).nonzero(as_tuple=True)[0].item()
     return idx2token_dict[idx]
 
-  
+def build_onehot_encoding_model(unk_ignore):
+    if unk_ignore == True:
+        model = preprocessing.OneHotEncoder(handle_unknown='ignore')
+    else:
+        model = preprocessing.OneHotEncoder()
+
+    return model
+
 def onehot_encoding(_encoder, _tks):
-    tk_list = [[x] for x in _tks[0]]
+    tk_list = [[x] for x in _tks]
     return [torch.tensor(x) for x in _encoder.transform(tk_list).toarray()]
